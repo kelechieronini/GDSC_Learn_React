f81072913316dc6b76f2079c3ec49a583d29e52e
Eronini Kelechi <kelechieronini@yahoo.com>
2023-03-09 20:59:11 +0100

    added confirmation pop-up for delete

---
+++ src/components/ConfirmModal.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const ConfirmModal = ({item, handleDelete, showConfirmContainer}) => {
+
+  return (
+    <div className="confirm-container">
+      <div className="confirm">
+        <p>Are you sure you want to delete this item?</p>
+        <div className="ask">
+          <button className="yes" onClick={() => handleDelete(item.id)}>
+            Yes
+          </button>
+          <button className="no" onClick={() => showConfirmContainer('')}>No</button>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default ConfirmModal;
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,22 +1,25 @@
+import React, {useState} from 'react'
 import Card from "../shared/Card";
 import { FaTimes } from 'react-icons/fa'
+import ConfirmModal from './ConfirmModal';
 
-function FeedbackItem({ item, feedback, setFeedback }) {
+const FeedbackItem = ({ item, feedback, setFeedback }) => {
 const handleDelete = (id) => {
   setFeedback(() => feedback.filter((arr) => arr.id !== id));
 }
 
-  return (
+ const [confirmContainer, showConfirmContainer] = useState('')
+
+  return(
     <Card className="card">
       <div className="num-display">{item.rating}</div>
-      <button className="close" onClick={() => handleDelete(item.id)}>
+      <button className="close" onClick={() => showConfirmContainer(item.id)}>
         <FaTimes color='blue'/>
       </button>
       <div className="text-display">{item.text}</div>
-    
+      {confirmContainer === item.id && <ConfirmModal item={item} handleDelete={handleDelete} showConfirmContainer={showConfirmContainer} />}
     </Card>
-    
-  );
+  )
 }
 
 export default FeedbackItem;
